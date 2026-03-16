ae1da461793dc978c0a46a9290a4d109f2e4e848
sohel mansoori <khansohel8960@gmail.com>
2026-03-17 01:37:24 +0530

    update deposit scanner and tron client

--- cripto_exchange-main/src/services/blockchain/tronClient.js
+++ cripto_exchange-main/src/services/blockchain/tronClient.js
@@ -1,26 +1,15 @@
-// import pkg from "tronweb";
 
-// const { TronWeb } = pkg;
-
-// const tronWeb = new TronWeb({
-//   fullHost: "https://api.trongrid.io", // ✅ MUST BE NILE
-//   headers: {
-//     "TRON-PRO-API-KEY": process.env.TRONGRID_API_KEY,
-//   },
-// });
-
-// export default tronWeb;
 
 import pkg from "tronweb";
 
 const { TronWeb } = pkg;
 
 const tronWeb = new TronWeb({
-  fullHost: "https://api.trongrid.io",
-  privateKey: process.env.PRIVATE_KEY,
+  fullHost: "https://api.trongrid.io",   // ✅ MAINNET
   headers: {
     "TRON-PRO-API-KEY": process.env.TRONGRID_API_KEY || "",
   },
 });
 
 export default tronWeb;
+
