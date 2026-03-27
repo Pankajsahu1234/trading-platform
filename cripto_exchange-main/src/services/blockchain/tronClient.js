b5798d16216aa6199431876522a40e3804d48fcb
Your Name <anubhavg.cse22@gmail.com>
2026-03-28 03:37:34 +0530

    tronclinet

--- cripto_exchange-main/src/services/blockchain/tronClient.js
+++ cripto_exchange-main/src/services/blockchain/tronClient.js
@@ -5,7 +5,7 @@ import pkg from "tronweb";
 const { TronWeb } = pkg;
 
 const tronWeb = new TronWeb({
-  fullHost: "https://nile.trongrid.io",   // ✅ MAINNET
+  fullHost: "https://api.trongrid.io",   // ✅ MAINNET
   headers: {
     "TRON-PRO-API-KEY": process.env.TRONGRID_API_KEY || "",
   },
